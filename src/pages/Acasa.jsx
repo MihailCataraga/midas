7821732654407c93ebff0a5787e1d2c52552d98d
Mihai <cataraga.95@gmail.com>
2024-03-16 18:14:09 +0200

    test animation

--- src/pages/Acasa.jsx
+++ src/pages/Acasa.jsx
@@ -1,13 +1,21 @@
 import React, { useEffect } from 'react';
-import Img1 from '../assets/imgs/Poza 1.png'
+import Img1 from '../assets/imgs/Poza 1.png';
+import Img2 from '../assets/imgs/Poza 2.png';
+import Img3 from '../assets/imgs/Poza 3.png';
 
 export default function Acasa() {
+    
   return (
     <div className='acasa'>
         <main>
             <div className='sec-2'>
                 <div className='imgsLeft'>
-                    <img id='img1' src={Img1} alt='Simbol img 1' />
+                    <img id='img3' src={Img1} alt='Simbol img 1' />
+                    <img id='img2' src={Img2} alt='Simbol img 2' />
+                    <img id='img1' src={Img3} alt='Simbol img 3' />
+                    {/* <div id='img1'></div>
+                    <div id='img2'></div>
+                    <div id='img3'></div> */}
                 </div>
                 <div className='textRight'>
                     <p>La Midas Group, credem că fiecare afacere merită să aibă un site web care să se ridice la standardele cele mai înalte de calitate și profesionalism. Suntem dedicați să creăm site-uri web personalizate și inovatoare pentru clienții noștri. Indiferent dacă sunteți o afacere mică sau o corporație mare, suntem aici pentru a vă ajuta să vă transformați viziunea în realitate digitală.</p>
